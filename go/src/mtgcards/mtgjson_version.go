56e033795a911de2f0666a92186f15c3547cb96a
Aaron Bamberger <aaron.bamberger@gmail.com>
2020-04-19 19:13:18 -0500

    Fix a card importer crash when version from mtgjson doesn't have a date
    The JSON unmarshalling code for the mtgjson version structure assumed
    that the version field would always have the date offset in addition to
    the semver version.  This isn't true for the first release of a new
    version, where the version field just has the semver version without a
    date offset.  This caused a crash in the JSON unmarshaller.  Since this
    is a valid format for the version field, this fixes the version JSON
    unmarshaller to accept this format as well.

--- go/src/mtgcards/mtgjson_version.go
+++ go/src/mtgcards/mtgjson_version.go
@@ -49,13 +49,22 @@ func (version *MTGJSONVersion) UnmarshalJSON(data []byte) error {
         return err
     }
 
+    // Version can either be just a semver version number,
+    // <major>.<minor>.<patch>
+    // or a semver with a date offset,
+    // <major>.<minor>.<patch>+<year><month><day>
+    // Handle both of those potential formats, with a missing date offset
+    // represented with a zero-valued time.Time object
     versionAndDate := strings.Split(dummyVersion.Version, "+")
-    version.VersionDate, err = time.Parse("20060102", versionAndDate[1])
-    if err != nil {
-        return err
+    if len(versionAndDate) < 1 {
+        return fmt.Errorf("Unexpected version string %s", dummyVersion.Version)
     }
 
+    // Split and parse the parts of the semver version string
     semverParts := strings.Split(versionAndDate[0], ".")
+    if len(semverParts) != 3 {
+        return fmt.Errorf("Unexpected semver string %s", versionAndDate[0])
+    }
 
     version.VersionMajor, err = strconv.Atoi(semverParts[0])
     if err != nil {
@@ -68,6 +77,15 @@ func (version *MTGJSONVersion) UnmarshalJSON(data []byte) error {
     version.VersionPatch, err = strconv.Atoi(semverParts[2])
     if err != nil {
         return err
+    }
+
+    if len(versionAndDate) == 2 {
+        version.VersionDate, err = time.Parse("20060102", versionAndDate[1])
+        if err != nil {
+            return err
+        }
+    } else {
+        version.VersionDate = time.Time{}
     }
 
     return nil
